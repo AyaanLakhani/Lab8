52c325e95c31f66467af934f739cdf02f0ecc908
EyeOnYou15 <96965326+EyeOnYou15@users.noreply.github.com>
2022-04-20 13:47:55 -0500

    Add files via upload

---
+++ Exercise4/Exercise4.js
@@ -0,0 +1,32 @@
+function ChangeBorderRed() {
+    var tag = document.getElementById("BC");
+      tag.style.borderColor = "red";
+    
+}
+function ChangeBorderGreen() {
+    var tag2 = document.getElementById("BC");
+      tag2.style.borderColor = "green";
+    
+}
+function ChangeBorderBlue() {
+    var tag3 = document.getElementById("BC");
+      tag3.style.borderColor = "blue";
+    
+}
+function ChangeBorderStyle() {
+    var tag3 = document.getElementById("BC");
+      tag3.style.border = "dotted";
+    
+}
+function ChangeBackgroundRed()
+{
+    document.body.style.backgroundColor = "red";
+}
+function ChangeBackgroundGreen()
+{
+    document.body.style.backgroundColor = "green";
+}
+function ChangeBackgroundBlue()
+{
+    document.body.style.backgroundColor = "blue";
+}
